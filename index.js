a30bda1ab5b4ec2f190d11667d64d296a3a7c6b6
lavy2p <lavy28p@gmail.com>
2021-06-09 15:36:17 -0500

    done axios call and initial data setup from api

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ const fetchData = async () => {
   try {
     const getInfo = await axios.get(domain);
     const farmInfo = getInfo.data;
-  // var farmInfo = JSON.parse(domain);
     console.log(farmInfo);
-    const finalInfo = farmInfo.slice(2,6);
+    const finalInfo = farmInfo.slice(0,200);
     
     
     console.log(finalInfo);
     
     finalInfo.forEach((array) => {
       
-      if ((typeof array.business != 'undefined')) {
+      if (array.farm_name && array.website ) {
         const item_name = array.item;
         console.log(`item : ${item_name}`);
 
-        const business_name = array.business;
-        console.log(`Business name : ${business_name}`);
+        addItem(item_name);
 
-        const coordinates = array.location_1.type;
-        console.log(`Coordinates : ${coordinates}`);
-      
+        const farm = array.farm_name;
+        console.log(`Farm name : ${farm}`);
+        
         const website = array.website.url;
         console.log(`url : ${website}`);
+
+        const address = `${array.location_1_address},${array.location_1_city},${array.location_1_state},${array.location_1_zip}`;
+        console.log(`Address : ${address}`);
         
       } 
    });
@@ -36,7 +37,16 @@ const fetchData = async () => {
 }
 
 fetchData();
+
 //dynamic get item value from api and add it in the in the dop down
+const addItem = (item_name) => {
+  const dropDown = document.querySelector('#fruits'); 
+  const optionItem = document.createElement('option');
+  optionItem.innerText = item_name;
+  dropDown.appendChild(optionItem);
+
+}
+
 //pick a value on drop down 
 // when clicked search , display farm names
 
